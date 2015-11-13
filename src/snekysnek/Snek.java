6b330f5419c7b990cddf85164b83584a08f583bd
Justinson <Justinson@Justins-MacBook-Pro-2.local>
2015-11-13 09:04:59 -0800

    2nd syncing

--- src/snekysnek/Snek.java
+++ src/snekysnek/Snek.java
@@ -18,7 +18,7 @@ import java.awt.event.MouseEvent;
  * @author Justinson
  */
 class Snek extends Environment {
-    
+
     Grid grid;
 
     public Snek() {
@@ -29,16 +29,48 @@ class Snek extends Environment {
     public void initializeEnvironment() {
     }
 
+    int counter;
+
     @Override
     public void timerTaskHandler() {
+//        System.out.println("Hey" + ++counter);
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
+        
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            System.out.println("Go left");
+        }
+        
+        else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            System.out.println("Go right");
+        }
+        
+        else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            System.out.println("Go up");
+        }
+        
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            System.out.println("Go down");
+        }
     }
 
     @Override
     public void keyReleasedHandler(KeyEvent e) {
+           if (e.getKeyCode() == KeyEvent.VK_W) {
+            System.out.println("GO UP");
+        }
+              if (e.getKeyCode() == KeyEvent.VK_A) {
+            System.out.println("GO LEFT");
+        }
+               if (e.getKeyCode() == KeyEvent.VK_S) {
+            System.out.println("GO DOWN");
+        }
+           if (e.getKeyCode() == KeyEvent.VK_D) {
+            System.out.println("GO RIGHT");
+        }
+
     }
 
     @Override
@@ -49,9 +81,9 @@ class Snek extends Environment {
 
     @Override
     public void paintEnvironment(Graphics graphics) {
-        if(grid != null){
+        if (grid != null) {
             grid.paintComponent(graphics);
         }
     }
-    
+
 }
